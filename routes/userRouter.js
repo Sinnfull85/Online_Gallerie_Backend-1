b5c3e8e0c6eac96bdf9c5a59d14b9a8a9b22133d
houtuantuan <80598603+houtuantuan@users.noreply.github.com>
2023-05-03 11:35:02 +0200

    endpoint for profile finished

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,10 +1,11 @@
 const { Router } = require('express')
 const userRouter = Router()
 const { protect } = require('../middlewares/auth')
-const { likeIt,getLikeIt } = require('../controllers/users')
+const { likeIt,getLikeIt,getAllLiked } = require('../controllers/users')
 
 
 userRouter.post('/like', protect, likeIt)
 .get("/like/:id",protect,getLikeIt)
+.get("/like",protect,getAllLiked)
 
 module.exports = userRouter
